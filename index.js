1f8a98bc3eee53f7d9a61c6b36beffa57aae6151
Wei Gao <jky239@gmail.com>
2014-08-25 18:28:58 +0800

    overload creating api

--- index.js
+++ index.js
@@ -3,8 +3,15 @@ var logger;
 
 
 module.exports = function(name, data) {
+  if (typeof name !== 'string' && data === undefined) {
+    data = name;
+    name = undefined;
+  }else if (name){
+    name = String(name);
+  }
+
   data = data || {};
-  if(!data.hasOwnProperty('component')) 
+  if(name && !data.hasOwnProperty('component')) 
     data.component = name;
   
   return logger.child(data);
